3370c5ae8ea84e998a4820d622415f1628fca94d
Valmir Souza <juniorformacao83@gmail.com>
2026-01-19 08:59:12 -0300

    retirada de aspas

--- config/database.js
+++ config/database.js
@@ -1,22 +1,22 @@
 const config = {
-  username: process.env.DB_USER || "postgres",
-  password: process.env.DB_PWD || "postgres",
-  database: process.env.DB_NAME || "postgres",
-  host: process.env.DB_HOST || "bia-db.c4nckw8qytev.us-east-1.rds.amazonaws.com",
+  username: process.env.DB_USER || 'postgres',
+  password: process.env.DB_PWD || 'postgres',
+  database: process.env.DB_NAME || 'postgres',
+  host: process.env.DB_HOST || 'bia-db.c4nckw8qytev.us-east-1.rds.amazonaws.com',
   port: process.env.DB_PORT || 5432,
-  dialect: "postgres",
+  dialect: 'postgres',
   logging: false,
   dialectOptions: {
     ssl: {
       require: true,
       // Esta é a linha que resolve o erro de self-signed certificate
-      rejectUnauthorized: false 
-    }
-  }
+      rejectUnauthorized: false,
+    },
+  },
 };
 
 module.exports = {
   development: config,
   test: config,
-  production: config
+  production: config,
 };
